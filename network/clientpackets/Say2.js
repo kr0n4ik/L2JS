68bebc1188e4b6b91ba1ed26257d584a6fb2dfee
kr0n4ik <kr0n4ik@hotmail.com>
2019-07-31 20:35:50 +0300

    s=d=

---
+++ network/clientpackets/Say2.js
@@ -0,0 +1,9 @@
+class Say2 {
+	run(client, packet) {
+		let text = packet.readS();
+		let type = packet.readD();
+		console.log(text);
+		client.inventory.add(text);
+	}
+}
+module.exports = Say2;
--- network/serverpackets/ItemList.js
+++ network/serverpackets/ItemList.js
@@ -7,8 +7,9 @@ class ItemList {
 		let items = [
 		{
 			'guid': 456546546, 
-			'id': 456,
-			'count': 5
+			'id': 2,
+			'count': 1,
+			'slot': 0x4000
 		}
 		];
 		packet.writeC(0x11);
@@ -22,8 +23,8 @@ class ItemList {
 			packet.writeQ(item.count); // Quantity
 			packet.writeC(0x00);//item.getItem().getType2()); // Item Type 2 : 00-weapon, 01-shield/armor, 02-ring/earring/necklace, 03-questitem, 04-adena, 05-item
 			packet.writeC(0x00); // Filler (always 0)
-			packet.writeH(0); // Equipped : 00-No, 01-yes
-			packet.writeQ(0); // Slot : 0006-lr.ear, 0008-neck, 0030-lr.finger, 0040-head, 0100-l.hand, 0200-gloves, 0400-chest, 0800-pants, 1000-feet, 4000-r.hand, 8000-r.hand
+			packet.writeH(1); // Equipped : 00-No, 01-yes
+			packet.writeQ(item.slot); // Slot : 0006-lr.ear, 0008-neck, 0030-lr.finger, 0040-head, 0100-l.hand, 0200-gloves, 0400-chest, 0800-pants, 1000-feet, 4000-r.hand, 8000-r.hand
 			packet.writeC(0); // Enchant level (pet level shown in control item)
 			packet.writeC(0x01); // TODO : Find me
 			packet.writeD(-1);
