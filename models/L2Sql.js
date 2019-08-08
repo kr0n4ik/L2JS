75576bb702b6305089f6f61241d6555cffce0bea
kr0n4ik <kr0n4ik@hotmail.com>
2019-08-08 07:40:01 +0300

    clear

--- models/L2Sql.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const util = require('util');
-class L2Sql {
-	constructor() {
-		this.db = mysql.createConnection({ host:'127.0.0.1', user:'lin', password:'123456', database:'l2j', insecureAuth: true});
-		this.query = util.promisify(this.db.query).bind(this.db);
-	}
-}
-module.exports = new L2Sql();
